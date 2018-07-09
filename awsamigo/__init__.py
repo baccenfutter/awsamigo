2f51ff80bc3be2985b594ef3d85e43d3c6ec0f60
Brian Wiborg <baccenfutter@c-base.org>
2018-07-10 01:06:35 +0200

    bump to 0.1.3

--- awsamigo/__init__.py
+++ awsamigo/__init__.py
@@ -6,7 +6,7 @@ from . import const
 from . import finder
 
 __authors__ = ["Brian Wiborg <baccenfutter@c-base.org>"]
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 def new_filter(filters=None):
     """
